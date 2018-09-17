03338d78e025de54144aebe11951b5b038205c62
Workshop <workshop@Olivers-MacBook-Air.local>
2018-09-17 16:14:28 +0100

    set up initial files for user login

--- routes/api.php
+++ routes/api.php
@@ -3,3 +3,8 @@
 $router->post("courses", "Courses@store");
 $router->get("courses", "Courses@index");
 
+
+
+$router->group(['middleware' => ['api','cors']], function ($router) {
+    $router->post('auth/register', 'Auth\RegisterController@create');
+ });
